dce4f51ad015fea7e1de7845a7b9cf267903f73c
Torkiwi <joachim.urbain@gmail.com>
2019-02-09 01:13:13 +0100

    initial upload

--- app/gilded-rose.ts
+++ app/gilded-rose.ts
@@ -10,6 +10,16 @@ export class Item {
     }
 }
 
+    const items_storage = {
+        'legendary':
+            ['Sulfuras, Hand of Ragnaros','Toast'],
+        'conjured':
+            ['Conjured Mana Cake'],
+        'passes':
+            ['Backstage passes to a TAFKAL80ETC concert'],
+        'aging':
+            ['Aged Brie']
+    }
 export class GildedRose {
     items: Array<Item>;
 
@@ -17,61 +27,111 @@ export class GildedRose {
         this.items = items;
     }
 
+
     updateQuality() {
         for (let i = 0; i < this.items.length; i++) {
             var item = this.items[i];
-            //Special Case Legendary
-            //- "Sulfuras", being a legendary item, never has to be sold or decreases in Quality
-            if (item.name == 'Sulfuras, Hand of Ragnaros') {
-                continue;
-            }
-            //- All items have a SellIn value which denotes the number of days we have to sell the item
-            switch (item.name) {
-                //- "Aged Brie" actually increases in Quality the older it gets
-                case 'Aged Brie':
+            var dec = 1
+
+            switch (type_of(item)) {
+                case 'legendary':
+                    continue;
+                
+                case 'aging':
                     item.quality++;
                     break;
 
-                //- "Backstage passes", like aged brie, increases in Quality as its SellIn value approaches;
                 case 'Backstage passes to a TAFKAL80ETC concert':
                     if (item.sellIn <= 0) {
-                        //Quality drops to 0 after the concert
-                        item.quality = 0;
-                    } else {
-                        //Quality increases by 2 when there are 10 days or less and by 3 when there are 5 days or less
-                        var inc = 1;
-                        if (item.sellIn <= 10) {
-                            inc = 2;
+                            item.quality = 0;
+                        } else {
+                            var inc = 1;
+                            if (item.sellIn <= 10) {
+                                inc = 2;
+                            }
+                            if (item.sellIn <= 5) {
+                                inc = 3;
+                            }
+                            item.quality += inc;
                         }
-                        if (item.sellIn <= 5) {
-                            inc = 3;
-                        }
-                        item.quality += inc;
-                    }
-                    break;
+                        break;
 
-                default:
-                    //- At the end of each day our system lowers quality for every item
-                    var dec = 1;
-                    if (item.name.indexOf('Conjured') != -1) {
-                        //- "Conjured" items degrade in Quality twice as fast as normal items
-                        dec = 2;
-                    }
+                case 'conjured':
+                    dec = 2;
+
+                default :
                     if (item.sellIn <= 0) {
-                        //- Once the sell by date has passed, Quality degrades twice as fast
-                        dec = dec * 2;
-                    }
-                    item.quality -= dec;
-                    break;
+                            dec = dec * 2;
+                        }
+                        item.quality -= dec;
+                        break;
             }
             item.sellIn--;
-            //- The Quality of an item is never negative
-            //- The Quality of an item is never more than 50
             item.quality = Math.max(0, Math.min(50, item.quality));
+   
+
+
+    //         //Special Case Legendary
+    //         //- "Sulfuras", being a legendary item, never has to be sold or decreases in Quality
+    //         if (item.name == 'Sulfuras, Hand of Ragnaros') {
+    //             continue;
+    //         }
+    //         //- All items have a SellIn value which denotes the number of days we have to sell the item
+    //         switch (item.name) {
+    //             //- "Aged Brie" actually increases in Quality the older it gets
+    //             case 'Aged Brie':
+    //                 item.quality++;
+    //                 break;
+
+    //             //- "Backstage passes", like aged brie, increases in Quality as its SellIn value approaches;
+    //             case 'Backstage passes to a TAFKAL80ETC concert':
+    //                 if (item.sellIn <= 0) {
+    //                     //Quality drops to 0 after the concert
+    //                     item.quality = 0;
+    //                 } else {
+    //                     //Quality increases by 2 when there are 10 days or less and by 3 when there are 5 days or less
+    //                     var inc = 1;
+    //                     if (item.sellIn <= 10) {
+    //                         inc = 2;
+    //                     }
+    //                     if (item.sellIn <= 5) {
+    //                         inc = 3;
+    //                     }
+    //                     item.quality += inc;
+    //                 }
+    //                 break;
+
+    //             default:
+    //                 //- At the end of each day our system lowers quality for every item
+    //                 var dec = 1;
+    //                 if (item.name.indexOf('Conjured') != -1) {
+    //                     //- "Conjured" items degrade in Quality twice as fast as normal items
+    //                     dec = 2;
+    //                 }
+    //                 if (item.sellIn <= 0) {
+    //                     //- Once the sell by date has passed, Quality degrades twice as fast
+    //                     dec = dec * 2;
+    //                 }
+    //                 item.quality -= dec;
+    //                 break;
+    //         }
+    //         item.sellIn--;
+    //         //- The Quality of an item is never negative
+    //         //- The Quality of an item is never more than 50
+    //         item.quality = Math.max(0, Math.min(50, item.quality));
         }
         return this.items;
     }
 }
+    function  type_of(item) {
+
+        for(var type in items_storage) {
+            if( items_storage[type].indexOf(item.name) != -1) {
+                return type;
+            }          
+ 
+        }
+   }
 
 const items = [
     new Item("+5 Dexterity Vest", 10, 20), //
@@ -88,7 +148,7 @@ const items = [
 
 
 const gildedRose = new GildedRose(items);
-var days: number = 10;
+var days: number = 2;
 for (let i = 0; i < days; i++) {
     console.log("-------- day " + i + " --------");
     console.log("name, sellIn, quality");
